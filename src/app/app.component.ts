157db561411756cfda9bc8a1fb2714b8210ee4c2
Ian Gerolamo <iangerolamo@gmail.com>
2022-03-29 21:14:49 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'oauth2-angular';
+
+  goToLink() {
+    window.location.href = 'https://stackoverflow.com';;
+  }
+  
 }
